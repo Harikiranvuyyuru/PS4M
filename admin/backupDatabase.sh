cd47293bd33c5b748761baabb26c01e9efbffdc9
TobyRoseman <ubuntu@ip-172-31-29-165.us-west-2.compute.internal>
2015-10-09 06:45:22 +0000

    Fixed DB backup

--- admin/backupDatabase.sh
+++ admin/backupDatabase.sh
@@ -5,5 +5,8 @@ set -x
 temp_file=/tmp/PS4M.db-$(date +\%m-\%d-\%Y)
 mysqldump --user=root --password=$DB_PASSWORD PS4M > $temp_file 2> /home/ubuntu/logs/dbBackup.txt
 gzip $temp_file
+source /etc/environment
 aws s3 cp ${temp_file}.gz s3://ps4m/databaseBackup/
 rm ${temp_file}.gz
+echo [Done]
+
